5f2ff7a2ceca9a4cbca2fa8e1034f38c783cd369
sammed-21 <sammed2110@gmail.com>
2023-03-13 15:49:21 +0530

    add

---
+++ client-app/README.md
@@ -0,0 +1,5 @@
+# Starter code for Faucet dApp tutorial
+
+Boilerplate code consisting of a new create-react-app project and some basic HTML and CSS.
+
+this is the faucet Decentralized application 
---
+++ client-app/src/App.js
@@ -0,0 +1,180 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ethers } from "ethers";
+import faucetContract from "./ethereum/faucet";
+
+function App() {
+  const [walletAddress, setWalletAddress] = useState("");
+  const [signer, setSigner] = useState();
+  const [fcContract, setFcContract] = useState();
+  const [withdrawError, setWithdrawError] = useState("");
+  const [withdrawSuccess, setWithdrawSuccess] = useState("");
+  const [transactionData, setTransactionData] = useState("");
+
+  useEffect(() => {
+    getCurrentWalletConnected();
+    addWalletListener();
+  }, [walletAddress]);
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        /* MetaMask is installed */
+        //web3Provider only if you are connect the metamask with ether .js library
+        //or normal with it we can do it in this way
+        //const accounts = await window.ethereum.request({method:eth_requestAccounts})
+        //console.log(accounts[0])
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        const accounts = await provider.send("eth_requestAccounts", []);
+        // get signer
+        setSigner(provider.getSigner());
+        //local contract instance
+
+        setFcContract(faucetContract(provider));
+
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        const accounts = await provider.send("eth_accounts", []);
+        // get signer
+        if (accounts.length > 0) {
+          setSigner(provider.getSigner());
+          //local contract instance
+
+          setFcContract(faucetContract(provider));
+          setWalletAddress(accounts[0]);
+          console.log(accounts[0]);
+        } else {
+          console.log("Connect to MetaMask using the Connect button");
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      });
+    } else {
+      /* MetaMask is not installed */
+      setWalletAddress("");
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const getOCTHandler = async () => {
+    setWithdrawError("");
+    setWithdrawSuccess("");
+    try {
+      //ethers: when we need to invoke a function that make transaction on blockchain we need to create the instance
+      //need to connect the contract instance with the signer
+      const fcContractSigner = fcContract.connect(signer);
+      const resp = await fcContractSigner.requestTokens();
+      console.log(resp);
+      setWithdrawSuccess("Operation success");
+      setTransactionData(resp.hash);
+    } catch (error) {
+      setWithdrawError(error.message);
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <h1 className="navbar-item is-size-4">Ocean Token(OCT)</h1>
+          </div>
+          <div id="navbarMenu" className="navbar-menu">
+            <div className="navbar-end is-align-items-center">
+              <button
+                className="button is-white connect-wallet"
+                onClick={connectWallet}
+              >
+                <span className="is-link has-text-weight-bold">
+                  {walletAddress && walletAddress.length > 0
+                    ? `Connected: ${walletAddress.substring(
+                        0,
+                        6
+                      )}...${walletAddress.substring(38)}`
+                    : "Connect Wallet"}
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <section className="hero is-fullheight">
+        <div className="faucet-hero-body">
+          <div className="container has-text-centered main-content">
+            <h1 className="title is-1 fon">Faucet</h1>
+            <p>Fast and reliable. 50 OCT/day.</p>
+            <div className="mt-5">
+              {withdrawError && (
+                <div className="withdraw-error">{withdrawError}</div>
+              )}
+              {withdrawSuccess && (
+                <div className="withdraw-success">{withdrawSuccess}</div>
+              )}
+            </div>
+            <div className="box address-box">
+              <div className="columns">
+                <div className="column is-four-fifths">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    placeholder="Enter your wallet address (0x...)"
+                    defaultValue={walletAddress}
+                  />
+                </div>
+                <div className="column">
+                  <button
+                    className="button is-link is-medium"
+                    onClick={getOCTHandler}
+                    disabled={walletAddress ? false : true}
+                  >
+                    GET TOKENS
+                  </button>
+                </div>
+              </div>
+              <article className="panel is-grey-darker">
+                <p className="panel-heading">Transaction Data</p>
+                <div className="panel-block">
+                  <p>
+                    {transactionData
+                      ? `'transaction hash' ${transactionData}`
+                      : "--"}
+                  </p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
